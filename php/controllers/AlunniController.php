49b922f9be18804c6ef9f63c7a25200337663937
lai2-24 <lai2-24@itismeucci.com>
2026-02-24 12:41:15 +0100

    AlunniController:show

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -12,4 +12,49 @@ class AlunniController
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function show(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $id = (int) $args['id'];
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id = $id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function create(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    /*$dati = $request->getParsedBody();
+    $nome = $dati['nome'];
+    $cognome = $dati['cognome'];*/
+    $nome = "tommaso";
+    $cognome = "faliani";
+    $idq = $mysqli_connection->query("SELECT MAX(id) FROM alunni");
+    $idr = $idq->fetch_all(MYSQLI_ASSOC);
+    $id = (int) $idr['id'];
+    $result = $mysqli_connection->query("INSERT INTO alunni(id, nome, cognome) VALUES ($id, $nome, $cognome)");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function update(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function destroy(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
 }
