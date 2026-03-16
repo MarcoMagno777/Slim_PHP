439617676502d8c6104827129d25c8fd1a85d76c
studente <studente@itismeucci.com>
2026-03-16 10:36:54 +0100

    certificazione_Iniziata

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -95,4 +95,107 @@ class AlunniController
     $response->getBody()->write(json_encode($response_data));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function indexCertificazioni(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $idAl = (int) $args['idAl'];
+    $result = $mysqli_connection->query("SELECT * FROM certificazioni WHERE alunno_id = $idAl");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function showCertificazioni(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $idAl = (int) $args['idAl'];
+    $idC = (int) $args['idC'];
+    $result = $mysqli_connection->query("SELECT * FROM alunni a JOIN certificazioni c ON a.id = c.alunno_id WHERE a.id = $idAl AND c.id = $idC");
+
+    if ($result) {
+        $results = $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        $results = [
+            'status' => 'error',
+            'message' => 'not found'
+        ];
+    }
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function createCertificazioni(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $dati = $request->getParsedBody();
+    $nome = $dati['nome'];
+    $cognome = $dati['cognome'];
+    $result = $mysqli_connection->query("INSERT INTO alunni(nome, cognome) VALUES ('$nome', '$cognome')");
+
+    if ($result) {
+        $response_data = [
+            'status' => 'success',
+            'id' => $id,
+            'nome' => $nome,
+            'cognome' => $cognome
+        ];
+    } else {
+        $response_data = [
+            'status' => 'error',
+            'message' => 'Failed to insert record'
+        ];
+    }
+
+    $response->getBody()->write(json_encode($response_data));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function updateCertificazioni(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $dati = $request->getParsedBody();
+    $id = (int) $args['id'];
+    $nome = $dati['nome'];
+    $cognome = $dati['cognome'];
+    $result = $mysqli_connection->query("UPDATE alunni SET nome = '$nome', cognome = '$cognome' WHERE id = $id");
+
+    if ($result) {
+        $response_data = [
+            'status' => 'success',
+            'id' => $id,
+            'nome' => $nome,
+            'cognome' => $cognome
+        ];
+    } else {
+        $response_data = [
+            'status' => 'error',
+            'message' => 'Failed to insert record'
+        ];
+    }
+
+    $response->getBody()->write(json_encode($response_data));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function destroyCertificazioni(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $id = (int) $args['id'];
+    $result = $mysqli_connection->query("DELETE FROM alunni WHERE id = $id");
+      if ($result) {
+        $response_data = [
+            'status' => 'success',
+            'id' => $id,
+            'nome' => $nome,
+            'cognome' => $cognome
+        ];
+    } else {
+        $response_data = [
+            'status' => 'error',
+            'message' => 'Failed to insert record'
+        ];
+    }
+
+    $response->getBody()->write(json_encode($response_data));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
 }
--- php/index.php
+++ php/index.php
@@ -7,10 +7,17 @@ require __DIR__ . '/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->addBodyParsingMiddleware();
+
 $app->get('/alunni', "AlunniController:index");
 $app->get('/alunni/{id}', "AlunniController:show");
 $app->post('/alunni', "AlunniController:create");
 $app->put('/alunni/{id}', "AlunniController:update");
 $app->delete('/alunni/{id}', "AlunniController:destroy");
+
+$app->get('/alunni/{idAl}/certificazioni', "AlunniController:indexCertificazioni");
+$app->get('/alunni/{idAl}/certificazioni/{idC}', "AlunniController:showCertificazioni");
+$app->post('/alunni/{idAl}/certificazioni', "AlunniController:createCertificazioni");
+$app->put('/alunni/{idAl}/certificazioni/{idC}', "AlunniController:updateCertificazioni");
+$app->delete('/alunni/{idAl}/certificazioni{idC}', "AlunniController:destroyCertificazioni");
 
 $app->run();
